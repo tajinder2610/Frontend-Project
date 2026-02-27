47a11d0ce420b214a0a2b5155fff040e460a9999
tajinder2610 <tajinderpalsingh26@gmail.com>
2026-02-27 15:56:29 +0530

    project update 3

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,37 +2,52 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import paginationSlice from "../redux/paginationSlice";
 import fetchMiddleware from "../redux/fetchMovieMiddleware";
+
 const actions = paginationSlice.actions;
 
 const Pagination = () => {
-    const {pageNo} = useSelector((store) => store.paginationState);
+  const { pageNo } = useSelector((store) => store.paginationState);
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
-    
-    const handlePrevious =() => {
-        dispatch(actions.handlePrevious())
-    }
+  const handlePrevious = () => {
+    if (pageNo > 1) dispatch(actions.handlePrevious());
+  };
 
-    const handleNext =() => {
-        //10000 movies , 20 movies pp , totoal pages -> 500 
-        dispatch(actions.handleNext())
-    }
+  const handleNext = () => {
+    dispatch(actions.handleNext());
+  };
 
-    useEffect(() => {
-        dispatch(fetchMiddleware(pageNo))
-    },[pageNo]);
-    
-    return (
-        <div className="flex justify-center gap-3 bg-gray-400 p-4 h-[50px] w-full mt-8">
-            <div onClick={handlePrevious} className="px-8 text-xl">
-            <i class="fa-solid fa-arrow-left"></i>
-            </div>
-            <div>{pageNo}</div>
-            <div onClick={handleNext} className="px-8 text-xl">
-            <i class="fa-solid fa-arrow-right"></i>
-            </div>
-        </div>
-    )
-}
+  useEffect(() => {
+    dispatch(fetchMiddleware(pageNo));
+  }, [pageNo, dispatch]);
+
+  return (
+    <div className="flex justify-center items-center gap-4 p-4 w-full mt-8">
+      
+      {/* Left Arrow */}
+      <div
+        className={`w-12 h-12 flex items-center justify-center text-white text-xl rounded-full cursor-pointer
+        ${pageNo === 1 ? "bg-transparent" : "bg-black"}`}
+        onClick={handlePrevious}
+      >
+        <i className={`fa-solid fa-arrow-left ${pageNo === 1 ? "invisible" : ""}`}></i>
+      </div>
+
+      {/* Page Number */}
+      <div className="w-12 h-12 flex items-center justify-center bg-black text-white text-lg font-semibold rounded-full">
+        {pageNo}
+      </div>
+
+      {/* Right Arrow */}
+      <div
+        className="w-12 h-12 flex items-center justify-center bg-black text-white text-xl rounded-full cursor-pointer"
+        onClick={handleNext}
+      >
+        <i className="fa-solid fa-arrow-right"></i>
+      </div>
+
+    </div>
+  );
+};
 
 export default Pagination;
